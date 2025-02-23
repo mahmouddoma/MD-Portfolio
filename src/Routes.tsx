be23db30560fe97b7b4caaaa5a05c1fe4fcb488d
mahmouddoma <devdoma2002@gmail.com>
2025-02-23 15:25:01 +0200

    sec deploy

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,21 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Hearo from "./components/Hero/Hero";
-import Projects from "./components/Projects/Projects";
-import Hero from "./components/Hero/Hero";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Hearo from "./components/Hero/Hero";
+// import Projects from "./components/Projects/Projects";
+// import Hero from "./components/Hero/Hero";
 
-const AppRoutes = () => {
-  return (
-    <Router>
-      <div className="app">
-        <Hearo />
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/hero" element={<Hero />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
+// const AppRoutes = () => {
+//   return (
+//     <Router>
+//       <div className="app">
+//         <Hearo />
+//         <Routes>
+//           <Route path="/" element={<Hero />} />
+//           <Route path="/hero" element={<Hero />} />
+//           <Route path="/projects" element={<Projects />} />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// };
 
-export default AppRoutes;
+// export default AppRoutes;
